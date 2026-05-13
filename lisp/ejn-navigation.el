8966fb078065c9a642fdf9bd58e29977d667c2a6
Kyohei-Prj <tatsukawa929@gmail.com>
2026-05-13 09:59:45 +0900

    feat: add cell navigation primitives and commands

---
+++ lisp/ejn-navigation.el
@@ -0,0 +1,156 @@
+;;; ejn-navigation.el --- Cell navigation commands  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025 Kyohei
+
+;; This file is part of emacs-jupyter-notebook.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Structural motion commands operating on cell boundaries.
+
+;;; Code:
+
+(require 'cl-lib)
+(require 'ejn-cell)
+(require 'ejn-model)
+(require 'ejn-render)
+
+(defun ejn--find-cell-id-at-point ()
+  "Return the cell ID text property at point.
+If point is not in a source region (e.g., in an output zone),
+scans backward to find the nearest cell ID."
+  (let ((cell-id (get-text-property (point) 'ejn-cell-id)))
+    (unless cell-id
+      (save-excursion
+        (while (and (> (point) (point-min)) (not cell-id))
+          (backward-char)
+          (setq cell-id (get-text-property (point) 'ejn-cell-id)))))
+    cell-id))
+
+(defun ejn-cell-at-point ()
+  "Return the `ejn-cell' struct at point, or signal an error.
+If point is in an output zone, finds the parent cell by scanning backward."
+  (let ((notebook (buffer-local-value 'ejn--notebook (current-buffer))))
+    (unless notebook
+      (user-error "Not in an EJN buffer"))
+    (let ((cell-id (ejn--find-cell-id-at-point)))
+      (unless cell-id
+        (user-error "Not in a cell"))
+      (ejn-notebook-cell-by-id notebook cell-id))))
+
+(defun ejn-cell-region ()
+  "Return (START . END) of the current cell's source region.
+Excludes the output zone."
+  (let ((cell-id (ejn--find-cell-id-at-point)))
+    (unless cell-id
+      (user-error "Not in a cell"))
+    (save-excursion
+      (goto-char (point-min))
+      (let ((start (point)))
+        (while (and (< start (point-max))
+                    (not (string= (get-text-property start 'ejn-cell-id)
+                                  cell-id)))
+          (setq start (next-single-property-change
+                       start 'ejn-cell-id nil (point-max))))
+        (when (< start (point-max))
+          (let ((end (next-single-property-change
+                      start 'ejn-cell-id nil (point-max))))
+            (cons start end)))))))
+
+(defun ejn-cell-full-region ()
+  "Return (START . END) of the current cell including output zone."
+  (let* ((source-region (ejn-cell-region))
+         (start (car source-region))
+         (end (cdr source-region)))
+    (save-excursion
+      (goto-char end)
+      (while (and (< (point) (point-max))
+                  (not (get-text-property (point) 'ejn-output-zone)))
+        (forward-char))
+      (when (get-text-property (point) 'ejn-output-zone)
+        (while (and (< (point) (point-max))
+                    (get-text-property (point) 'ejn-output-zone))
+          (forward-char))
+        (setq end (point))))
+    (cons start end)))
+
+(defun ejn--goto-cell-by-id (cell-id)
+  "Move point to the end of the cell with CELL-ID."
+  (goto-char (point-min))
+  (while (and (< (point) (point-max))
+              (not (string= (get-text-property (point) 'ejn-cell-id) cell-id)))
+    (forward-char))
+  (when (string= (get-text-property (point) 'ejn-cell-id) cell-id)
+    (while (and (< (point) (point-max))
+                (string= (get-text-property (point) 'ejn-cell-id) cell-id))
+      (forward-char))))
+
+(defun ejn-goto-next-cell ()
+  "Move point to the start of the next cell's source region."
+  (interactive)
+  (let ((current-id (get-text-property (point) 'ejn-cell-id))
+        (next-id nil))
+    (unless current-id
+      (setq current-id (ejn--find-cell-id-at-point)))
+    (save-excursion
+      (goto-char (1+ (point)))
+      (while (and (< (point) (point-max)) (not next-id))
+        (let ((id (get-text-property (point) 'ejn-cell-id)))
+          (when (and id (not (string= id current-id)))
+            (setq next-id id)))
+        (forward-char)))
+    (if next-id
+        (ejn--goto-cell-by-id next-id)
+      (user-error "Already at last cell"))))
+
+(defun ejn-goto-prev-cell ()
+  "Move point to the start of the previous cell's source region."
+  (interactive)
+  (let ((current-id (get-text-property (point) 'ejn-cell-id))
+        (prev-id nil))
+    (unless current-id
+      (setq current-id (ejn--find-cell-id-at-point)))
+    (save-excursion
+      (while (and (> (point) (point-min)) (not prev-id))
+        (backward-char)
+        (let ((id (get-text-property (point) 'ejn-cell-id)))
+          (when (and id (not (string= id current-id)))
+            (setq prev-id id)))))
+    (if prev-id
+        (ejn--goto-cell-by-id prev-id)
+      (user-error "Already at first cell"))))
+
+(defun ejn-goto-first-cell ()
+  "Move point to the start of the first cell."
+  (interactive)
+  (goto-char (point-min)))
+
+(defun ejn-goto-last-cell ()
+  "Move point to the start of the last cell's source region."
+  (interactive)
+  (let ((last-id nil))
+    (save-excursion
+      (goto-char (1- (point-max)))
+      (while (and (> (point) (point-min)) (not last-id))
+        (let ((id (get-text-property (point) 'ejn-cell-id)))
+          (when id (setq last-id id)))
+        (backward-char)))
+    (if last-id
+        (ejn--goto-cell-by-id last-id)
+      (goto-char (point-min)))))
+
+(provide 'ejn-navigation)
+;;; ejn-navigation.el ends here
---
+++ test/ejn-navigation-test.el
@@ -0,0 +1,137 @@
+;;; ejn-navigation-test.el --- Tests for ejn-navigation  -*- lexical-binding: t; -*-
+
+(require 'ert)
+(require 'ejn-navigation)
+(require 'ejn-model)
+(require 'ejn-render)
+(require 'ejn-test-util)
+
+(ert-deftest ejn-navigation-test/cell-at-point-returns-cell ()
+  "ejn-cell-at-point should return the cell struct at point."
+  (let ((nb (ejn-make-notebook)))
+    (ejn-notebook-insert-cell nb 'code :at 0)
+    (ejn-notebook-set-cell-source nb (ejn-cell-id (ejn-notebook-cell-at-index nb 0)) "print(1)")
+    (ejn-test-with-temp-buffer " *test*"
+			       (set (make-local-variable 'ejn--notebook) nb)
+			       (ejn-render-notebook nb)
+			       (goto-char (point-min))
+			       (let ((cell (ejn-cell-at-point)))
+				 (should (ejn-cell-p cell))
+       (should (string= (ejn-cell-source cell) "print(1)"))))))
+
+(ert-deftest ejn-navigation-test/cell-at-point-in-output-zone ()
+  "ejn-cell-at-point should find parent cell from within output zone."
+  (require 'ejn-cell)
+  (let ((nb (ejn-make-notebook)))
+    (ejn-notebook-insert-cell nb 'code :at 0)
+    (let* ((cell (ejn-notebook-cell-at-index nb 0))
+           (cell-id (ejn-cell-id cell)))
+      (setf (ejn-cell-source cell) "42")
+      (setf (ejn-cell-outputs cell)
+            (list (make-ejn-output
+                   :type 'execute-result
+                   :mime-data (list :data (list (cons 'text/plain (list "42"))))
+                   :metadata nil
+                   :request-id nil)))
+      (ejn-test-with-temp-buffer " *test*"
+        (set (make-local-variable 'ejn--notebook) nb)
+        (ejn-render-notebook nb)
+        (search-forward "42\n42" nil t)
+        (forward-line)
+        (let ((found-cell (ejn-cell-at-point)))
+          (should (ejn-cell-p found-cell))
+          (should (string= (ejn-cell-id found-cell) cell-id)))))))
+
+(ert-deftest ejn-navigation-test/cell-region-returns-source-range ()
+  "ejn-cell-region should return the source region boundaries."
+  (let ((nb (ejn-make-notebook)))
+    (ejn-notebook-insert-cell nb 'code :at 0)
+    (ejn-notebook-set-cell-source nb (ejn-cell-id (ejn-notebook-cell-at-index nb 0)) "line1\nline2")
+    (ejn-test-with-temp-buffer " *test*"
+			       (set (make-local-variable 'ejn--notebook) nb)
+			       (ejn-render-notebook nb)
+			       (goto-char (point-min))
+			       (let ((region (ejn-cell-region)))
+				 (should (= (car region) (point-min)))
+				 (should (> (cdr region) (car region)))))))
+
+(ert-deftest ejn-navigation-test/cell-full-region-includes-output ()
+  "ejn-cell-full-region should include the output zone."
+  (require 'ejn-cell)
+  (let ((nb (ejn-make-notebook)))
+    (ejn-notebook-insert-cell nb 'code :at 0)
+    (let ((cell (ejn-notebook-cell-at-index nb 0)))
+      (setf (ejn-cell-source cell) "x")
+      (setf (ejn-cell-outputs cell)
+            (list (make-ejn-output
+                   :type 'execute-result
+                   :mime-data (list :data (list (cons 'text/plain (list "result"))))
+                   :metadata nil
+                   :request-id nil)))
+      ) ;; end let cell
+    (ejn-test-with-temp-buffer " *test*"
+			       (set (make-local-variable 'ejn--notebook) nb)
+			       (ejn-render-notebook nb)
+			       (goto-char (point-min))
+			       (let ((source-region (ejn-cell-region))
+				     (full-region (ejn-cell-full-region)))
+				 (should (> (cdr full-region) (cdr source-region)))))))
+
+(ert-deftest ejn-navigation-test/goto-next-cell-moves-forward ()
+  "ejn-goto-next-cell should move to the next cell's source."
+  (let ((nb (ejn-make-notebook)))
+    (ejn-notebook-insert-cell nb 'code :at 0)
+    (ejn-notebook-set-cell-source nb (ejn-cell-id (ejn-notebook-cell-at-index nb 0)) "first")
+    (ejn-notebook-insert-cell nb 'code :at 1)
+    (ejn-notebook-set-cell-source nb (ejn-cell-id (ejn-notebook-cell-at-index nb 1)) "second")
+    (ejn-test-with-temp-buffer " *test*"
+      (set (make-local-variable 'ejn--notebook) nb)
+      (ejn-render-notebook nb)
+      (goto-char (point-min))
+      (ejn-goto-next-cell)
+      (should (search-backward "second" nil t)))))
+
+(ert-deftest ejn-navigation-test/goto-prev-cell-moves-backward ()
+  "ejn-goto-prev-cell should move to the previous cell's source."
+  (let ((nb (ejn-make-notebook)))
+    (ejn-notebook-insert-cell nb 'code :at 0)
+    (ejn-notebook-set-cell-source nb (ejn-cell-id (ejn-notebook-cell-at-index nb 0)) "first")
+    (ejn-notebook-insert-cell nb 'code :at 1)
+    (ejn-notebook-set-cell-source nb (ejn-cell-id (ejn-notebook-cell-at-index nb 1)) "second")
+    (ejn-test-with-temp-buffer " *test*"
+      (set (make-local-variable 'ejn--notebook) nb)
+      (ejn-render-notebook nb)
+      (search-forward "second")
+      (ejn-goto-prev-cell)
+      (should (search-backward "first" nil t)))))
+
+(ert-deftest ejn-navigation-test/goto-first-cell ()
+  "ejn-goto-first-cell should move to the first cell."
+  (let ((nb (ejn-make-notebook)))
+    (ejn-notebook-insert-cell nb 'code :at 0)
+    (ejn-notebook-set-cell-source nb (ejn-cell-id (ejn-notebook-cell-at-index nb 0)) "first")
+    (ejn-notebook-insert-cell nb 'code :at 1)
+    (ejn-notebook-set-cell-source nb (ejn-cell-id (ejn-notebook-cell-at-index nb 1)) "second")
+    (ejn-test-with-temp-buffer " *test*"
+      (set (make-local-variable 'ejn--notebook) nb)
+      (ejn-render-notebook nb)
+      (goto-char (point-max))
+      (ejn-goto-first-cell)
+      (should (= (point) (point-min))))))
+
+(ert-deftest ejn-navigation-test/goto-last-cell ()
+  "ejn-goto-last-cell should move to the last cell."
+  (let ((nb (ejn-make-notebook)))
+    (ejn-notebook-insert-cell nb 'code :at 0)
+    (ejn-notebook-set-cell-source nb (ejn-cell-id (ejn-notebook-cell-at-index nb 0)) "first")
+    (ejn-notebook-insert-cell nb 'code :at 1)
+    (ejn-notebook-set-cell-source nb (ejn-cell-id (ejn-notebook-cell-at-index nb 1)) "last")
+    (ejn-test-with-temp-buffer " *test*"
+      (set (make-local-variable 'ejn--notebook) nb)
+      (ejn-render-notebook nb)
+      (goto-char (point-min))
+      (ejn-goto-last-cell)
+      (should (search-backward "last" nil t)))))
+
+(provide 'ejn-navigation-test)
+;;; ejn-navigation-test.el ends here
